fea38e2e89f8d76850c0440226a875749e618144
Sido Haakma <sido@haakma.org>
2021-09-16 10:25:26 +0200

    fix: ci directory references

--- .azure/build_docker.bash
+++ .azure/build_docker.bash
@@ -10,7 +10,7 @@
 # - GITHUB_TOKEN: semantic release uses this environment variable to push to github
 
 echo "Set the docker authentication configuration"
-DOCKER_CONFIG=${AGENT_HOMEDIRECTORY}/.docker
+DOCKER_CONFIG="${AGENT_HOMEDIRECTORY}/.docker"
 mkdir -p "${DOCKER_CONFIG}"
 set +x && echo "{\"auths\": \"https://index.docker.io/v1/\": {\"auth\": \"${DOCKERHUB_AUTH}\"}, \"registry.hub.docker.com\": {\"auth\": \"${DOCKERHUB_AUTH}\"}}}" > "${DOCKER_CONFIG}/config.json"
 
@@ -18,7 +18,7 @@ set +x && echo "{\"auths\": \"https://index.docker.io/v1/\": {\"auth\": \"${DOCK
 envs=($(ls -d */))
 for env in "${envs[@]}"
 do
-  cd "${$AGENT_BUILDDIRECTORY}"
+  cd "${AGENT_BUILDDIRECTORY}"
   cd "${env}"
   echo "Building for environment: [ ${env} ]"
   images=($(ls -d */))
